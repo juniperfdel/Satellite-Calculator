8f23cc583574492940ea3bf6f3357e3d3f159b97
Greg Foote <gregoryf@udel.edu>
2024-02-29 14:43:42 -0500

    test_oom fleshed out

--- test_oom.py
+++ test_oom.py
@@ -24,27 +24,38 @@ def guess_type(file: Path) -> FileGuess:
     return file_type
 
 
-omm_parsers = {FileGuess.csv: omm.parse_csv, FileGuess.xml: omm.parse_xml}
+def main(ifmt: str = "tle"):
+    omm_parsers = {FileGuess.csv: omm.parse_csv, FileGuess.xml: omm.parse_xml}
 
-ts = load.timescale()
-active = Path("active.temp")
+    ts = load.timescale()
+    active = Path(f"active.{ifmt}")
 
-if not active.exists():
-    data = urlopen(
-        "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=csv"
-    )
-    active.write_bytes(data.read())
+    if not active.exists():
+        data = urlopen(
+            f"https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT={ifmt}"
+        )
+        active.write_bytes(data.read())
 
 
-ext = guess_type(active)
+    ext = guess_type(active)
 
-sats = []
-with open(active) as f:
-    for fields in omm_parsers[ext](f):
-        sat = Satrec()
-        omm.initialize(sat, fields)
-        nsat = EarthSatellite.from_satrec(sat, ts)
-        nsat.name = fields["OBJECT_NAME"]
-        sats.append(nsat)
+    sats = []
 
-print(sats)
+    if ext == FileGuess.tle:
+        sats = load.tle_file(str(active))
+    else:
+        with open(active) as f:
+            for fields in omm_parsers[ext](f):
+                sat = Satrec()
+                omm.initialize(sat, fields)
+                nsat = EarthSatellite.from_satrec(sat, ts)
+                nsat.name = fields["OBJECT_NAME"]
+                sats.append(nsat)
+
+    print(sats)
+
+
+if __name__ == "__main__":
+    main("tle")
+    main("xml")
+    main("csv")
